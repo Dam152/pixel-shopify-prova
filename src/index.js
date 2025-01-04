3bcc9b503740947da8dfaaa1a27f4b7991c00dff
Damiano Ciucci <damjaguar98@icloud.com>
2025-01-04 14:37:10 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Alpine from "alpinejs";
+import "../assets/loghi-slider.js";
+import "../assets/featured-collection-slider.js";
+import Aos, { init } from "aos";
+import "aos/dist/aos.css";
+
+window.Alpine = Alpine;
+Alpine.start();
+Aos.init();
